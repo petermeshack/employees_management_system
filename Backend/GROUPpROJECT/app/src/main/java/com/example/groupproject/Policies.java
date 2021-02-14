8a4d63335ebece5166716e28ee703cd91662d6d3
peter_meshack <peter_meshack@bitbucket.org>
2021-02-14 15:38:37 +0300

    added login , singup to firebase

--- Backend/GROUPpROJECT/app/src/main/java/com/example/groupproject/Policies.java
+++ Backend/GROUPpROJECT/app/src/main/java/com/example/groupproject/Policies.java
@@ -2,13 +2,54 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Policies extends AppCompatActivity {
+    ImageView toHomep, toProfilep, toLeavesp, toConversationp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_policies);
+
+        toHomep = findViewById(R.id.HomeIvp);
+        toProfilep = findViewById(R.id.ProfileIvp);
+        toLeavesp = findViewById(R.id.LeavesIvp);
+
+
+
+        toHomep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+        toProfilep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+        toLeavesp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+        toConversationp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+
+
     }
 }
