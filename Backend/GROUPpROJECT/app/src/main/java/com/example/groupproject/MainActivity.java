2bc0fc6111db4893d91fb206705669ed238baeea
peter_meshack <peter_meshack@bitbucket.org>
2021-03-01 10:39:36 +0300

    added more code on the layout

--- Backend/GROUPpROJECT/app/src/main/java/com/example/groupproject/MainActivity.java
+++ Backend/GROUPpROJECT/app/src/main/java/com/example/groupproject/MainActivity.java
@@ -2,13 +2,109 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView toHome, toProfile, toLeaves, toPolicies, toLogout;
+    private FirebaseAuth mAuth;
+    TextView BasicInfo, AllocateDuty, DepartmentM, ManageEmployee;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        BasicInfo = findViewById(R.id.BasicInfotxt);
+        AllocateDuty =findViewById(R.id.AllocationInfotxt);
+        DepartmentM = findViewById(R.id.DependentsInfotxt);
+        ManageEmployee = findViewById(R.id.ManageEMPInfotxt);
+
+        toHome = findViewById(R.id.HomeIv);
+        toProfile = findViewById(R.id.ProfileIv);
+        toLeaves = findViewById(R.id.LeavesIv);
+        toPolicies = findViewById(R.id.PoliciesIv);
+        toLogout = findViewById(R.id.LogoutIv);
+
+
+        BasicInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EmployeeProfile.class));
+                finish();
+            }
+        });
+
+        AllocateDuty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Duties.class));
+                finish();
+            }
+        });
+
+        ManageEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EmployeeRegistration.class));
+                finish();
+            }
+        });
+
+        AllocateDuty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Duties.class));
+                finish();
+            }
+        });
+
+
+
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+        toProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Search.class));
+                finish();
+            }
+        });
+        toLeaves.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Leaves.class));
+                finish();
+            }
+        });
+        toPolicies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Policies.class));
+                finish();
+            }
+        });
+
+        toLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Splashscreen1.class));
+                finish();
+            }
+        });
     }
-}
+    }
---
+++ Backend/GROUPpROJECT/app/src/main/java/com/example/groupproject/Splashscreen1.java
@@ -0,0 +1,42 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Splashscreen1 extends AppCompatActivity {
+    FirebaseAuth.AuthStateListener firebaseAuthStateListenner;
+    FirebaseAuth mAuth;
+    String accountType;
+    String TAG = "calculateWorkTime";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen1);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        int SPLASH_DISPLAY_LENGTH = 2000;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+
+                if(FirebaseAuth.getInstance().getCurrentUser() != null){
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }else{
+                    startActivity(new Intent(getApplicationContext(), SplashScreen.class));
+                }
+                finish();
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
